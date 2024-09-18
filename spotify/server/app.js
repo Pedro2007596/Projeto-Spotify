fe5118b6985a366dbd2c50b82c788f600bd5da97
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 08:43:19 -0300

    fazendo mongo db

---
+++ spotify/server/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import conectaNaDb from './db.js';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+const conexao = await conectaNaDb();
+
+conexao.on('error', (erro) => {
+    console.error('Erro ao conectar no MongoDB', erro)
+})
+conexao.once('open', () => {
+    console.log('Conectado ao MongoDB')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor conectado')
+})
