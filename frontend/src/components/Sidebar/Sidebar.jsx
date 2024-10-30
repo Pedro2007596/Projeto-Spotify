5b2c1ecf16ad44069bd0b1f725085629334e9cc1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-30 07:51:05 -0300

    organização a mais

---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import CardNull from "../Cards/CardNull"
+import ContainerSidebar from "./ContainerSidebar"
+
+function Sidebar(){
+    return(
+        <ContainerSidebar>
+          <CardNull>
+              <h5>
+              Alguma Coisa <br /> <br />
+            MAS EU ESTOU COM PREGUIÇA <br/><br /> :D
+              </h5>
+          </CardNull>
+        </ContainerSidebar>
+    )
+}
+export default Sidebar
