c1c83021742bc28db8aedd0067bbe6ccb052b69d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-01 08:30:01 -0300

    db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 
 export default async function conectaNaDb(){
-    mongoose.connect("mongodb+srv://adm:adm@adm.arqu1.mongodb.net/Spotfy?retryWrites=true&w=majority&appName=adm")
+    mongoose.connect("mongodb+srv://b4rr1gameplay:Pedro2007@spotify.smm0e.mongodb.net/Spotify?retryWrites=true&w=majority&appName=Spotify")
     return mongoose.connection;
 }
